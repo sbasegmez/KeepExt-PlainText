516402b4e55bb60df69ea6186edf05df6bb378db
Serdar <serdar.basegmez@gmail.com>
2024-05-15 12:52:12 +0100

    Initial commit

---
+++ src/main/java/com/developi/keep/ext/PlainTextExtension.java
@@ -0,0 +1,47 @@
+package com.developi.keep.ext;
+
+import com.hcl.domino.DominoClient;
+import com.hcl.domino.data.Document;
+import com.hcl.domino.data.Item;
+import com.hcl.domino.data.ItemDataType;
+import com.hcl.domino.keep.info.richtext.impl.PlainRichtextProcessor;
+import com.hcl.domino.mime.MimeData;
+import io.vertx.core.json.JsonObject;
+import org.jsoup.Jsoup;
+
+import java.util.Optional;
+
+public class PlainTextExtension extends PlainRichtextProcessor {
+
+    @Override
+    public JsonObject process(DominoClient dominoClient, Document document, String itemName) {
+        Optional<ItemDataType> optItemType = document.getFirstItem(itemName)
+                                                     .map(Item::getType);
+
+        if (optItemType.isPresent() && optItemType.get() == ItemDataType.TYPE_MIME_PART) {
+            // We only fix MIME part behaviour for now.
+            MimeData mimeData = document.get(itemName, MimeData.class, null);
+            if (null != mimeData) {
+                String plainText = mimeData.getPlainText();
+
+                if (null == plainText || plainText.isEmpty()) {
+                    // If there is no plain text, we need to use HTML
+                    plainText = Jsoup.parse(mimeData.getHtml())
+                                     .text();
+                }
+
+                return JsonObject.of("type", "text/plain",
+                        "encoding", "PLAIN",
+                        "content", plainText
+                );
+            }
+        }
+
+        return super.process(dominoClient, document, itemName);
+    }
+
+    @Override
+    public int getPriority() {
+        return 5; // Higher number means higher priority
+    }
+}
---
+++ src/test/java/com/developi/keep/ext/PlainTextExtensionTest.java
@@ -0,0 +1,124 @@
+package com.developi.keep.ext;
+
+import com.developi.domino.AbstractNotesRuntimeTest;
+import com.hcl.domino.DominoClient;
+import com.hcl.domino.commons.org.apache.commons.mail.Email;
+import com.hcl.domino.commons.org.apache.commons.mail.HtmlEmail;
+import com.hcl.domino.commons.org.apache.commons.mail.SimpleEmail;
+import com.hcl.domino.commons.util.StringUtil;
+import com.hcl.domino.data.Document;
+import com.hcl.domino.mime.MimeWriter;
+import io.vertx.core.json.JsonObject;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//@Disabled("Not ready")
+class PlainTextExtensionTest extends AbstractNotesRuntimeTest {
+
+    private final static String TEXT_CONTENT = "Lorem Ipsum dolor sit amet";
+    private final static String HTML_CONTENT = "Lorem\n<div>Ipsum <br>\n<b> dolor</b></div>";
+    private final static String HTML_CONTENT_EXPECTED = "Lorem Ipsum dolor";
+
+
+    @Test
+    void test_MimeWithTextOnly() throws Exception {
+        final PlainTextExtension extension = new PlainTextExtension();
+
+        this.withTempDb(database -> {
+            DominoClient client = getClient();
+            Document document = database.createDocument();
+
+            createMime(document, "Body", TEXT_CONTENT, null);
+
+            JsonObject result = extension.process(getClient(), document, "Body");
+
+            // type and encoding can be tested once.
+            assertEquals(result.getString("type"), "text/plain");
+            assertEquals(result.getString("encoding"), "PLAIN");
+
+            assertEquals(result.getString("content"), TEXT_CONTENT);
+        });
+    }
+
+    @Test
+    void test_MimeWithHtmlOnly() throws Exception {
+        final PlainTextExtension extension = new PlainTextExtension();
+
+        this.withTempDb(database -> {
+            DominoClient client = getClient();
+            Document document = database.createDocument();
+
+            createMime(document, "Body", null, HTML_CONTENT);
+
+            JsonObject result = extension.process(getClient(), document, "Body");
+
+            assertEquals(result.getString("content"), HTML_CONTENT_EXPECTED);
+        });
+    }
+
+    @Test
+    void test_MimeWithTextAndHtml() throws Exception {
+        final PlainTextExtension extension = new PlainTextExtension();
+
+        this.withTempDb(database -> {
+            DominoClient client = getClient();
+            Document document = database.createDocument();
+
+            createMime(document, "Body", TEXT_CONTENT, HTML_CONTENT);
+
+            JsonObject result = extension.process(getClient(), document, "Body");
+
+            assertEquals(result.getString("content"), TEXT_CONTENT);
+        });
+    }
+
+    private void createMime(Document doc, String fieldName, String textPlain, String textHtml) throws Exception {
+        // Create MimeWriter
+        MimeWriter mw = doc.getParentDatabase()
+                           .getParentDominoClient()
+                           .getMimeWriter();
+
+        Email email;
+        MimeMessage mimeMessage;
+
+        boolean text = StringUtil.isNotEmpty(textPlain);
+        boolean html = StringUtil.isNotEmpty(textHtml);
+
+        if (text && html) {
+            // Multipart
+            email = new HtmlEmail();
+            ((HtmlEmail) email).setTextMsg(textPlain);
+            ((HtmlEmail) email).setHtmlMsg(textHtml);
+        } else {
+            email = new SimpleEmail();
+            if (text) {
+                email.setContent(textPlain, "text/plain; charset=UTF-8");
+            } else {
+                email.setContent(textHtml, "text/html; charset=UTF-8");
+            }
+        }
+
+        email.setHostName("localhost");
+        email.setFrom("test@example.com");
+        email.setTo(Arrays.asList(InternetAddress.parse("test@example.com")));
+
+        email.buildMimeMessage();
+
+        mimeMessage = email.getMimeMessage();
+
+        mw.writeMime(
+                doc,
+                fieldName,
+                mimeMessage,
+                EnumSet.of(MimeWriter.WriteMimeDataType.BODY, MimeWriter.WriteMimeDataType.HEADERS)
+        );
+    }
+
+
+}
